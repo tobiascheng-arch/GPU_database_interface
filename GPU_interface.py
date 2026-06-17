573af3c39e27eb74b189a6f1600ede24b96eabfa
tobiascheng <tobiascheng@riccarton.school.nz>
2026-06-18 11:26:26 +1200

    3rd commit - set up that structure of the code, going to turn it all into an interface that is usable

--- GPU_interface.py
+++ GPU_interface.py
@@ -5,6 +5,10 @@ from easygui import *
 
 # This is the filename of the database to be used
 DB_NAME = 'GPU_database.db'
+TABLES = ("GPU_database"
+        "LEFT JOIN Interfaces ON GPU_database.Interface_id = Interfaces.Interface_id"
+        "LEFT JOIN Manufacture ON GPU_database.Manufacture_ID = Manufacture.Manufacture_ID"
+        "LEFT JOIN Memory ON GPU_database.Memory_id = Memory.Memory_id")
 
 #Print query function - prints out existing queries from sqlite
 def print_query(view_name:str):
@@ -23,11 +27,57 @@ def print_query(view_name:str):
     codebox("Here are the results of your query:", "Query results", tabulate(results,headings))
     db.close()
 
-choice = ''
+def print_parameter_query(fields:str, where:str, parameter):
+    """ Prints the results for a parameter query in tabular form. """
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    sql = ("SELECT " + fields + " FROM " + TABLES + " WHERE " + where)
+    cursor.execute(sql,(parameter,))
+    results = cursor.fetchall()
+    codebox("Here are the results:", "Results:", tabulate(results,fields.split(",")))
+    db.close()  
+
 while True:
     msg ="What do you want to see?"
-    title = "Music lessons"
-    choices = ["All_info", "Avg_to_good_performance", "GPUs_below_or_equal_$500", "NVIDIA_with_great_upscaling_gen", "Top_10_cheapest", "Top_10_expensive", "Value_for_money", "avg_to_cheap_NVIDIA"]
+    title = "GPU database"
+    choices = ["Queries", "Manufacturer", "Model", "VRAM", "Memory type", "Bus (in bits)", "Release year", "GPU boost clock (MHz)","Power (W)", "Interface", "Length", "Ray tracing", "Price range", "Performance rated by ai", "Upscaling Gen" ]
     choice = choicebox(msg, title, choices)
-    if choice == None: sys.exit()
-    print_query(choice)
+
+    if choice == "Queries":
+        msg ="What query do you want to see?"
+        title = "GPU database"
+        query_choices = ["All_info", "Avg_to_good_performance", "GPUs_below_or_equal_$500", "NVIDIA_with_great_upscaling_gen", "Top_10_cheapest", "Top_10_expensive", "Value_for_money", "avg_to_cheap_NVIDIA"]
+        query = choicebox(msg, title, query_choices)
+        if choice == None: sys.exit()
+        print_query(query)
+    
+    elif choice == "Manufacturer":
+        print(1)
+    elif choice == "Model":
+        print(2)
+    elif choice == "VRAM":
+        print(3)
+    elif choice == "Memory type":
+        print(4)
+    elif choice == "Bus (in bits)":
+        print(5)
+    elif choice == "Release year":
+        print(6)
+    elif choice == "GPU boost clock (MHz)":
+        print(7)
+    elif choice == "Power (W)":
+        print(8)
+    elif choice == "Interface":
+        print(9)
+    elif choice == "Length":
+        print(10)
+    elif choice == "Ray tracing":
+        print(11)
+    elif choice == "Price range":
+        print(12)
+    elif choice == "Performance rated by ai":
+        print(13)
+    elif choice == "Upscaling Gen":
+        print(14)
+    else:
+        sys.exit()
