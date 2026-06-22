385a9ebde58ea500581c7164501d16a2cd2784aa
tobiascheng <tobiascheng@riccarton.school.nz>
2026-06-22 15:00:46 +1200

    5th commit - adding all of the info, nearly finished it, plus about to get feedback

--- GPU_interface.py
+++ GPU_interface.py
@@ -1,9 +1,10 @@
+#All of the imports used in this code is stores here
 import sqlite3
 from tabulate import tabulate
 import sys
 from easygui import *
 
-# This is the filename of the database to be used
+# Contains the file name, and the script used to connect all of the specific tables in the relational database
 DB_NAME = 'GPU_database.db'
 TABLES = (" GPU_database "
         "LEFT JOIN Interfaces ON GPU_database.Interface_id = Interfaces.Interface_id "
@@ -28,6 +29,8 @@ def print_query(view_name:str):
     codebox("Here are the results of your query:", "Query results", tabulate(results,headings))
     db.close()
 
+
+#Print parameter function - prints out a table based on the info you give it, e.g. a model that your interested in
 def print_parameter_query(fields:str, where:str, parameter):
     """ Prints the results for a parameter query in tabular form. """
     db = sqlite3.connect(DB_NAME)
@@ -36,8 +39,21 @@ def print_parameter_query(fields:str, where:str, parameter):
     cursor.execute(sql,(parameter,))
     results = cursor.fetchall()
     codebox("Here are the results:", "Results:", tabulate(results,fields.split(",")))
+    db.close()
+#Print parameter function with 2 parameters instead - does the same as the first one but has two parameters e.g. min and max limit for cost
+def print_parameter_query2(fields:str, where:str, parameter, parameter2):
+    """ Prints the results for a parameter query in tabular form. """
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    sql = ("SELECT " + fields + " FROM " + TABLES + " WHERE " + where)
+    cursor.execute(sql,(parameter, parameter2, ))
+    results = cursor.fetchall()
+    codebox("Here are the results:", "Results:", tabulate(results,fields.split(",")))
     db.close()  
 
+#added ALL_FIELDS to make it look clewaner
+ALL_FIELDS = "Manufacturer, Model, VRAM_GB, Memory_type, Bus_bit, Release_year, GPU_boost_clock_MHz, Power_W, Interface, Length_mm, Ray_tracing, Price_USD, Performance_rated_by_ai, Upscaling_gen"
+#While loop that loops until "x" or cancel is pressed, loops through asking t
 while True:
     msg ="What do you want to see?"
     title = "GPU database"
@@ -58,79 +74,145 @@ while True:
         choices = ["NVIDIA", "AMD", "Intel"]
         choice = choicebox(msg, title, choices)
         if choice == None: sys.exit()
-        print_parameter_query("Manufacturer, Model, [VRAM_(GB)], Memory_type, [Bus_(bit)], Release_year, [GPU_boost_clock_(MHz)], [Power_(W)], Interface, [Length_(mm)], Ray_tracing, [Price_(USD)], [Performance_(rated_by_ai)], Upscaling_gen", 
+        print_parameter_query(ALL_FIELDS, 
                             "Manufacturer = ? ORDER BY Model Asc",choice)
 
 
     elif choice == "Model":
         msg ="What model do you want to see?"
-        title = "GPU database"
-        choices = [""]
+        title = "GPU dataxbase"
+        choices = ["RTX", "GTX", "RX", "Arc"]
         choice = choicebox(msg, title, choices)
         if choice == None: sys.exit()
-        print_parameter_query("Manufacturer, Model, [VRAM_(GB)], Memory_type, [Bus_(bit)], Release_year, [GPU_boost_clock_(MHz)], [Power_(W)], Interface, [Length_(mm)], Ray_tracing, [Price_(USD)], [Performance_(rated_by_ai)], Upscaling_gen", 
-                            "Manufacturer = ? ORDER BY Model Asc",choice)
+        print_parameter_query(ALL_FIELDS, 
+                            "Model LIKE ? ORDER BY Model Asc", f"%{choice}%")
 
     elif choice == "VRAM":
-        msg ="How much Vram do you need?"
+        msg ="How much Vram do you need? (4 GB - 32 GB)\nPick your minimum:"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
-        if choice == None: sys.exit()
+        choice1 = integerbox(msg, title, default=8,
+                        lowerbound= 4,
+                        upperbound=32)
+        if choice1 != 32:
+            msg = f"Now pick your maximum (Between {choice1} and 32):"
+            choice2 = integerbox(msg, title, default=choice1,
+                            lowerbound=choice1,
+                            upperbound=32)
+        elif choice1 == 32: choice2 = 32
+        if choice1 == None or choice2 == None: sys.exit()
+        print_parameter_query2(ALL_FIELDS, "VRAM_GB BETWEEN ? AND ? ORDER BY VRAM_GB DESC, Model ASC", choice1, choice2)
+
     elif choice == "Memory type":
         msg ="What type of memory do you want to see?"
         title = "GPU database"
-        choices = [""]
+        choices = ["GDDR7", "GDDR6X", "GDDR6", "GDDR5", "GDDR5X"]
         choice = choicebox(msg, title, choices)
         if choice == None: sys.exit()
+        print_parameter_query(ALL_FIELDS, 
+                            "Memory_type = ? ORDER BY Memory_type Asc, Model ASC", choice)
+
     elif choice == "Bus (in bits)":
-        msg ="How much do you need?"
+        msg ="Pick your minimum between 64 & 512:"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
-        if choice == None: sys.exit()
+        choice1 = integerbox(msg, title, default=64,
+                        lowerbound= 64,
+                        upperbound=512)
+        if choice1 != 512:
+            msg = f"Now pick your maximum (Between {choice1} and 512):"
+            choice2 = integerbox(msg, title, default=choice1,
+                            lowerbound=choice1,
+                            upperbound=512)
+        elif choice1 == 512: choice2 = 512
+        if choice1 == None or choice2 == None: sys.exit()
+        print_parameter_query2(ALL_FIELDS, "Bus_bit BETWEEN ? AND ? ORDER BY VRAM_GB DESC, Model ASC", choice1, choice2)
+
     elif choice == "Release year":
-        msg ="What year do you want to see?"
+        msg ="Type down the year you want to see, (make sure it is between 2016 and2025):"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
+        choice = integerbox(msg, title, default=2016,
+                        lowerbound= 2016,
+                        upperbound= 2025)
         if choice == None: sys.exit()
+        print_parameter_query(ALL_FIELDS, "Release_year = ? ORDER BY Manufacturer ASC, Model ASC", choice)
+
     elif choice == "GPU boost clock (MHz)":
-        msg ="What do you want to see?"
+        msg ="Pick your minimum between 1582MHz & 2815MHz:"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
-        if choice == None: sys.exit()
+        choice1 = integerbox(msg, title, default=1582,
+                        lowerbound= 1582,
+                        upperbound=2815)
+        if choice1 != 2815:
+            msg = f"Now pick your maximum (Between {choice1}MHz and 2815MHz):"
+            choice2 = integerbox(msg, title, default=choice1,
+                            lowerbound=choice1,
+                            upperbound=2815)
+        elif choice1 == 2815: choice2 = 2815
+        if choice1 == None or choice2 == None: sys.exit()
+        print_parameter_query2(ALL_FIELDS, "GPU_boost_clock_MHz BETWEEN ? AND ? ORDER BY GPU_boost_clock_MHz DESC, Model ASC", choice1, choice2)
+
     elif choice == "Power (W)":
-        msg ="How efficient do you want it?"
+        msg ="Pick your minimum between 53W & 600W:"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
-        if choice == None: sys.exit()
+        choice1 = integerbox(msg, title, default=53,
+                        lowerbound= 53,
+                        upperbound=600)
+        if choice1 != 600:
+            msg = f"Now pick your maximum (Between {choice1}W and 600W):"
+            choice2 = integerbox(msg, title, default=choice1,
+                            lowerbound=choice1,
+                            upperbound=600)
+        elif choice1 == 600: choice2 = 600
+        if choice1 == None or choice2 == None: sys.exit()
+        print_parameter_query2(ALL_FIELDS, "Power_W BETWEEN ? AND ? ORDER BY Power_W ASC, Model ASC", choice1, choice2)
+
     elif choice == "Interface":
-        msg ="What do you want to see?"
+        msg ="Pick an interface"
         title = "GPU database"
-        choices = [""]
+        choices = ["PCIe 5.0 x16", "PCIe 4.0 x16", "PCIe 4.0 x8", "PCIe 3.0 x16", "PCIe 4.0 x4"]
         choice = choicebox(msg, title, choices)
         if choice == None: sys.exit()
+        print_parameter_query(ALL_FIELDS, "Interface = ? ORDER BY Interface ASC, Model ASC", choice)
+
+        
     elif choice == "Length":
-        msg ="What size do you want to see?"
+        msg ="Pick your minimum size between 170mm & 345mm:"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
-        if choice == None: sys.exit()
+        choice1 = integerbox(msg, title, default=170,
+                        lowerbound= 170,
+                        upperbound=345)
+        if choice1 != 345:
+            msg = f"Now pick your maximum (Between {choice1}mm and 345mm):"
+            choice2 = integerbox(msg, title, default=choice1,
+                            lowerbound=choice1,
+                            upperbound=345)
+        elif choice1 == 345: choice2 = 345
+        if choice1 == None or choice2 == None: sys.exit()
+        print_parameter_query2(ALL_FIELDS, "Length_mm BETWEEN ? AND ? ORDER BY Length_mm ASC, Model ASC", choice1, choice2)
+
+
     elif choice == "Ray tracing":
         msg ="Do you want Ray tracing?"
         title = "GPU database"
         choices = ["Yes", "No"]
         choice = choicebox(msg, title, choices)
         if choice == None: sys.exit()
+        print_parameter_query(ALL_FIELDS, "Ray_tracing = ? ORDER BY Manufacturer ASC, Model ASC", choice)
+
     elif choice == "Price range":
-        msg ="What query do you want to see?"
+        msg ="Pick your minimum price between 119 & 1999 USD:"
         title = "GPU database"
-        choices = [""]
-        choice = choicebox(msg, title, choices)
-        if choice == None: sys.exit()
+        choice1 = integerbox(msg, title, default=119,
+                        lowerbound= 119,
+                        upperbound=1999)
+        if choice1 != 1999:
+            msg = f"Now pick your maximum (Between {choice1} and 1999 USD):"
+            choice2 = integerbox(msg, title, default=choice1,
+                            lowerbound=choice1,
+                            upperbound=1999)
+        elif choice1 == 1999: choice2 = 1999
+        if choice1 == None or choice2 == None: sys.exit()
+        print_parameter_query2(ALL_FIELDS, "Price_USD BETWEEN ? AND ? ORDER BY PRICE_USD ASC, Performance_rated_by_ai DESC", choice1, choice2)
+
     elif choice == "Performance rated by ai":
         msg ="How fast do you want your GPU?"
         title = "GPU database"
